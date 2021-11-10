b9a7e9eb5eddcb9f7e9ba315f24061ea9548abda
JSarabia-dev <jsarabia.dev@gmail.com>
2021-11-10 12:56:41 -0300

    add rendering page .hbs

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+/* eslint-disable no-console */
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`, (err) => { console.error(err); });
 
 // Servir contenido estatico
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Wena mundo');
+  res.render('home', {
+    nombre: 'Juan Sarabia',
+    titulo: 'Curso de Node',
+  });
+});
+
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    nombre: 'Juan Sarabia',
+    titulo: 'Curso de Node',
+  });
+});
+
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    nombre: 'Juan Sarabia',
+    titulo: 'Curso de Node',
+  });
 });
 
 app.get('*', (req, res) => {
